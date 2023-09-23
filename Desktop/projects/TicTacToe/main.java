9bb37f9ca67a2e7ba507b18935fdfd5bec854edd
Khushi Sharma <Khushisharma20190@gmail.com>
2023-09-23 10:43:38 +0530

    0X game

---
+++ Desktop/projects/TicTacToe/main.java
@@ -0,0 +1,11 @@
+package TicTacToe;
+public class main
+    {
+        public static void main(String[] args) throws InterruptedException
+        {
+        TicTacToe ttt = new TicTacToe();
+        }
+
+    }
+
+    
